a38c82b45e48fdc1d5f994e748d24bd9125ceed2
sudo <sudharsansci@gmail.com>
2021-03-09 03:29:05 -0500

    refactor

--- ROSserver/rci_ws/build/robot_monitor/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/codezen/ROSserver/rci_ws/src/robot_monitor
-# Build directory: /home/codezen/ROSserver/rci_ws/build/robot_monitor
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- RobotClient/robot_ws/build/data_publisher/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/codezen/ROSRobot/robot_ws/src/data_publisher
-# Build directory: /home/codezen/ROSRobot/robot_ws/build/data_publisher
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
